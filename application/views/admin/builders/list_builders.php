51a0136396ed557190ae2bd45158273489f0e5be
samansinghal88 <singhprem166@gmail.com>
2021-08-06 21:53:14 +0530

    dsn

---
+++ application/views/admin/builders/list_builders.php
@@ -0,0 +1,94 @@
+<div class="row">
+   <div class="col-md-8 col-sm-9">
+      <div class="card">
+         <div class="card-header">
+            <h5 class="card-title">Add Builder</h5>
+         </div>
+         <div class="card-body">
+            <form method="POST" action="" enctype="multipart/form-data" accept-charset="utf-8">
+               <div class="mb-3">
+                  <label class="form-label">Builder Name</label>
+                  <input name="" type="text" class="form-control" placeholder="Enter builder name">
+               </div>
+			   <div class="mb-3">
+                  <label class="form-label">Builder Website</label>
+                  <input name="" type="text" class="form-control" placeholder="Enter builder website">
+               </div>
+			   <div class="mb-3">
+                  <label class="form-label">Established Year</label>
+                  <input name="" type="text" class="form-control" placeholder="Enter estd. year">
+               </div>
+			   <div class="mb-3">
+                  <label class="form-label">About Builder</label>
+                  <textarea name="" class="form-control" placeholder="Builder description here..." rows="3"></textarea>
+               </div>
+               <div class="mb-3">
+                  <label class="form-label">Builder Logo</label>
+                  <input type="file" class="form-control" name="" required>
+                  <div class="text-muted">Note : .png, .jpg, .jpeg, .webp, .svg format accepted</div>
+               </div>
+			   <div class="mb-3">
+                  <label class="form-label">Office Address</label>
+                  <textarea name="" class="form-control" placeholder="Office address" rows="2"></textarea>
+               </div>
+			   <div class="mb-3">
+                  <label class="form-label">Phone Numbers</label>
+                  <input name="" type="text" class="form-control" placeholder="Enter phone numbers">
+               </div>
+			   <div class="mb-3">
+                  <label class="form-label">Owner Name</label>
+                  <input name="" type="text" class="form-control" placeholder="Enter owner name">
+               </div>
+			   <div class="card-header">
+            <h5 class="card-title">Contact Person Details</h5>
+         </div>
+		 <table class="table">
+			<tr>
+			<th>SNo.</th>
+			<th>Name</th>
+			<th>Phone Number</th>
+			<th>Email ID</th>
+			<th></th>
+			</tr>
+			<tr>
+			<td>1</td>
+			<td><input name="" type="text" class="form-control form-control-sm" placeholder="Enter name"></td>
+			<td><input name="" type="text" class="form-control form-control-sm" placeholder="Enter phone no."></td>
+			<td><input name="" type="text" class="form-control form-control-sm" placeholder="Enter email id"></td>
+			<td>
+			<a href="javascript:;" style="opacity:.5;" disabled>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash align-middle">
+                           <polyline points="3 6 5 6 21 6"></polyline>
+                           <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                        </svg>
+                     </a>
+			</td>
+			</tr>
+			<tr>
+			<td>2</td>
+			<td><input name="" type="text" class="form-control form-control-sm" placeholder="Enter name"></td>
+			<td><input name="" type="text" class="form-control form-control-sm" placeholder="Enter phone no."></td>
+			<td><input name="" type="text" class="form-control form-control-sm" placeholder="Enter email id"></td>
+			<td>
+			<a href="javascript:;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash align-middle">
+                           <polyline points="3 6 5 6 21 6"></polyline>
+                           <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                        </svg>
+                     </a>
+			</td>
+			</tr>
+		 </table>
+		 <div class="mb-3">
+                 <a href="javascript:;">
+				 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+					<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+				</svg> Add Row</a>
+               </div>
+               <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+         </div>
+
+      </div>
+   </div>
+   </div>
