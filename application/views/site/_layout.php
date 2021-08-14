481fca2c9caf5df289cbc5d8baf957b32893b485
samansinghal88 <singhprem166@gmail.com>
2021-08-14 16:36:44 +0530

    dsn

--- application/views/site/_layout.php
+++ application/views/site/_layout.php
@@ -1,8 +1,70 @@
 <!--Main Header-->
-<?php include_once('_header.php'); ?>
+<!--<?php //include_once('_header.php'); ?>-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="keywords" content="">
+	
+	<link rel="shortcut icon" href="<?= base_url(); ?>assets/images/favicon.png" />
+
+	<title><?= $title; ?> - <?= $site_name; ?></title>
+	<link href="<?= base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?= base_url(); ?>assets/css/mulish.css" rel="stylesheet">
+	<link href="<?= base_url(); ?>assets/css/bootstrap-icons.css" rel="stylesheet">
+	<link href="<?= base_url(); ?>assets/css/custom.css" rel="stylesheet">
+	
+	<script src="<?= base_url(); ?>assets/js/jquery.min.js"></script>
+	<script src="<?= base_url(); ?>assets/js/bootstrap.bundle.min.js"></script>
+	
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+		<img class="ws-logo" src="<?= base_url(); ?>assets/images/logo.svg" />
+	</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+	  <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="javascript:;" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Buy in Gr. Noida
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="javascript:;">Noida</a></li>
+            <li><a class="dropdown-item" href="javascript:;">Greater Noida</a></li>
+			<li><a class="dropdown-item" href="javascript:;">Yamuna Expressway</a></li>
+			<li><a class="dropdown-item" href="javascript:;">Ghaziabad</a></li>
+            <li><a class="dropdown-item" href="javascript:;">Delhi</a></li>
+			<li><a class="dropdown-item" href="javascript:;">Gurgaon</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Offers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:;">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 <!-- *****CONTENT***** -->
 <main class="main-content">
     <?= $sub_view; ?>
 </main>
+
+<footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
 <!--Main Footer-->
-<?php include_once('_footer.php'); ?>
+<!--<?php //include_once('_footer.php'); ?>-->
--- application/views/site/pages/home.php
+++ application/views/site/pages/home.php
@@ -1,1 +1,248 @@
-<h1>Home Page</h1>
+<section class="section top-area">
+<div class="container text-center">    
+  <div class="row justify-content-center">
+    <div class="col-xl-8 col-lg-9 col-md-10 col-sm-11">
+      <h1 class="tb-title">Beautiful spaces in the best places</h1>
+	  <div class="srch-box mt-5 mb-5">
+		<div class="tab-list d-flex">
+			<a class="tab-link active" href="javascript:;">Flat</a>
+			<a class="tab-link" href="javascript:;">Plot</a>
+			<a class="tab-link" href="javascript:;">Residential</a>
+			<a class="tab-link" href="javascript:;">Commercial</a>
+			<a class="tab-link" href="javascript:;">Industrial</a>
+			<a class="tab-link" href="javascript:;">Villa</a>
+		</div>
+		<div class="input-group mb-2">
+			<select class="form-select form-select-lg mx-wd-150" id="inputGroupSelect01">
+			<option selected>Noida</option>
+			<option value="1">Greater Noida</option>
+			<option value="2">Ghaziabad</option>
+			<option value="3">Delhi</option>
+		  </select>
+		  <input type="text" class="form-control form-control-lg" placeholder="Search for locality, property type, project, builder...">
+		  <button class="btn btn-lg btn-primary" type="button">Search</button>
+		</div>
+	  </div>
+    </div>
+  </div>
+</div>
+
+</section>
+  
+<section class="section prop-loc-bys">
+<div class="container">
+    <div class="row mx-auto my-auto justify-content-center">
+    <h2 class="cmn-title">Property in India</h2>
+        <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="<?= base_url(); ?>assets/images/cities/noida.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Noida</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                            <img src="<?= base_url(); ?>assets/images/cities/greater-noida.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Greater Noida</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                            <img src="<?= base_url(); ?>assets/images/cities/yamuna-expressway.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Yamuna Expressway</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                            <img src="<?= base_url(); ?>assets/images/cities/ghaziabad.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Ghaziabad</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                            <img src="<?= base_url(); ?>assets/images/cities/delhi.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Delhi</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                            <img src="<?= base_url(); ?>assets/images/cities/gurgaon.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Gurgoun</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                            <img src="<?= base_url(); ?>assets/images/cities/faridabad.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Faridabad</div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            </a>
+            <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            </a>
+        </div>
+    </div>
+</div>
+
+</section>
+
+
+<section class="section fndhm">
+<div class="container">
+  <div class="row justify-content-center">
+      <div class="col-xl-6 col-lg-7 col-md-8 text-center">
+      <h3 class="pg-title">Find your Home Fast!</h3>
+      <h2 class="pg-title-big text-primary mb-4">With Propvenues!</h2>
+<p class="text-hdln">Just regist to be our member, let us know your full information and then buy your
+home to the most profitable budget!</p>
+      </div>
+  </div>
+</div>
+
+</section>
+
+
+<section class="section prop-loc-bys">
+<div class="container">
+    <div class="row mx-auto my-auto justify-content-center">
+    <h2 class="cmn-title">Latest Residential Flats</h2>
+        <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="<?= base_url(); ?>assets/images/cities/noida.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Noida</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                            <img src="<?= base_url(); ?>assets/images/cities/greater-noida.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Greater Noida</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                            <img src="<?= base_url(); ?>assets/images/cities/yamuna-expressway.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Yamuna Expressway</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                            <img src="<?= base_url(); ?>assets/images/cities/ghaziabad.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Ghaziabad</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                            <img src="<?= base_url(); ?>assets/images/cities/delhi.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Delhi</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                            <img src="<?= base_url(); ?>assets/images/cities/gurgaon.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Gurgoun</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-img">
+                            <img src="<?= base_url(); ?>assets/images/cities/faridabad.jpg" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay"><div class="ct-title">Project in<br />Faridabad</div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            </a>
+            <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            </a>
+        </div>
+    </div>
+</div>
+
+</section>
+
+
+
+<script>
+  $(document).ready(function(){
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 6
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+});
+});
+  </script>
+
