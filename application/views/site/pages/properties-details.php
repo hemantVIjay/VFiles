e779ef8ba2b237a561df763f4357c9cc67820582
samansinghal88 <singhprem166@gmail.com>
2021-09-04 15:43:25 +0530

    dsn

--- application/views/site/pages/properties-details.php
+++ application/views/site/pages/properties-details.php
@@ -429,46 +429,11 @@
 <div class="page-section" id="5">
 			<h4 class="cmn-title mb-4">Price Trends</h4>
 </div>
-<div class="page-section" id="6">
-			<h4 class="cmn-title mb-4">Ratings & Reviews</h4>
-			<div class="rtnrvw">
-				<div class="row">
-        <div class="col-md-6">
-                <div class="d-flex"> 
-                    <div class="rtstr">
-					<h5 class="rtttl">Rate us</h5>
-                    <fieldset class="rating"> 
-						<input type="radio" id="star5" name="rating" value="5" />
-						<label class="full" for="star5" title="5 stars"></label> 
-						<input type="radio" id="star4half" name="rating" value="4.5" />
-						<label class="half" for="star4half" title="4.5 stars"></label> 
-						<input type="radio" id="star4" name="rating" value="4" />
-						<label class="full" for="star4" title="4 stars"></label> 
-						<input type="radio" id="star3half" name="rating" value="3.5" />
-						<label class="half" for="star3half" title="3.5 stars"></label> 
-						<input type="radio" id="star3" name="rating" value="3" />
-						<label class="full" for="star3" title="3 stars"></label> 
-						<input type="radio" id="star2half" name="rating" value="2.5" />
-						<label class="half" for="star2half" title="2.5 stars"></label> 
-						<input type="radio" id="star2" name="rating" value="2" />
-						<label class="full" for="star2" title="2 stars"></label> 
-						<input type="radio" id="star1half" name="rating" value="1.5" />
-						<label class="half" for="star1half" title="1.5 stars"></label> 
-						<input type="radio" id="star1" name="rating" value="1" />
-						<label class="full" for="star1" title="1 star"></label> 
-						<input type="radio" id="starhalf" name="rating" value="0.5" />
-						<label class="half" for="starhalf" title="0.5 stars"></label> 
-						<input type="radio" class="reset-option" name="rating" value="reset" /> 
-					</fieldset>
-                </div>
-				<span class="myratings">4.5</span>
-				</div>
-        </div>
-    </div>
-			</div>
-</div>
+
 <div class="page-section" id="7">
 			<h4 class="cmn-title mb-4">About Developer</h4>
+			<div class="pvpd-desc">Imperia Prideville is an upcoming housing project at Yeida in Yeida, being developed by Imperia Structures Ltd. Keeping in mind the needs of modern lifestyle of residents, the project provides various amenities. Imperia Prideville encompasses amenities such as Restaurant , Children's Play Area, Spa/Sauna/Steam, Gymnasium and Golf Course. ATMS, medical facilities, education institutions, and parks are within easy reach. The area is also well-connected by bus and road network.<p></p>
+			</div>
 </div>
 <div class="bnksec">
 			<h4 class="cmn-title mb-4">Approved by Banks</h4>
@@ -505,6 +470,52 @@
 				</div>
 			</div>
 </div>
+
+<div class="page-section" id="6">
+			<h4 class="cmn-title mb-4">Ratings & Reviews</h4>
+			<div class="rtnrvw">
+				<div class="row">
+        <div class="col-xl-8 col-lg-9 col-md-10">
+                <div class="d-flex mb-3"> 
+                    <div class="rtstr">
+					<h6 class="rtttl">Rate us</h6>
+                    <fieldset class="rating"> 
+						<input type="radio" id="star5" name="rating" value="5" />
+						<label class="full" for="star5" title="5 stars"></label> 
+						<input type="radio" id="star4half" name="rating" value="4.5" />
+						<label class="half" for="star4half" title="4.5 stars"></label> 
+						<input type="radio" id="star4" name="rating" value="4" />
+						<label class="full" for="star4" title="4 stars"></label> 
+						<input type="radio" id="star3half" name="rating" value="3.5" />
+						<label class="half" for="star3half" title="3.5 stars"></label> 
+						<input type="radio" id="star3" name="rating" value="3" />
+						<label class="full" for="star3" title="3 stars"></label> 
+						<input type="radio" id="star2half" name="rating" value="2.5" />
+						<label class="half" for="star2half" title="2.5 stars"></label> 
+						<input type="radio" id="star2" name="rating" value="2" />
+						<label class="full" for="star2" title="2 stars"></label> 
+						<input type="radio" id="star1half" name="rating" value="1.5" />
+						<label class="half" for="star1half" title="1.5 stars"></label> 
+						<input type="radio" id="star1" name="rating" value="1" />
+						<label class="full" for="star1" title="1 star"></label> 
+						<input type="radio" id="starhalf" name="rating" value="0.5" />
+						<label class="half" for="starhalf" title="0.5 stars"></label> 
+						<input type="radio" class="reset-option" name="rating" value="reset" /> 
+					</fieldset>
+                </div>
+				<span class="myratings">0.0</span>
+				</div>
+
+				<textarea class="form-control" rows="4" placeholder="Comment type here..."></textarea>
+        </div>
+
+    </div>
+			</div>
+
+				
+
+</div>
+
 </div>
 </div>
 
