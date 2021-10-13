bc51263653f58152aa00ebea2556662aa6ea1f79
Prem Singh <61229245+singhprem166@users.noreply.github.com>
2021-10-13 21:06:00 +0530

    dsn

--- application/views/site/_layout.php
+++ application/views/site/_layout.php
@@ -414,6 +414,40 @@
             </div>
          </div>
       </div>
+	  
+	  <!-- Toast Code Start -->
+	  <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+		  <div class="toast align-items-center text-white bg-success border-0" role="alert" aria-live="assertive" aria-atomic="true">
+			  <div class="d-flex">
+				<div class="toast-body">
+				  Hello, Success! This is a toast message.
+				</div>
+				<button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+			  </div>
+			</div>
+			
+			<div class="toast align-items-center text-white bg-danger border-0" role="alert" aria-live="assertive" aria-atomic="true">
+			  <div class="d-flex">
+				<div class="toast-body">
+				  Hello, Failed! This is a toast message.
+				</div>
+				<button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+			  </div>
+			</div>
+			
+			<div class="toast align-items-center text-white bg-warning border-0" role="alert" aria-live="assertive" aria-atomic="true">
+			  <div class="d-flex">
+				<div class="toast-body">
+				  Hello, Warning! This is a toast message.
+				</div>
+				<button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+			  </div>
+			</div>
+			
+		</div>
+		
+		<!-- Toast Code End -->
+	  
 <!--Main Footer-->
 <!--<?php //include_once('_footer.php'); ?>-->
       <script>
@@ -508,5 +542,11 @@
 			  });	  
 		 return false;
 	} 
+	
+	
+	//Toast
+	$(document).ready(function(){
+	  $('.toast').toast('show');
+	});
    
 </script>
